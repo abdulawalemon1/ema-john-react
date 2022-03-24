f51ad9093f760a9d78020c600e8c55f009e7b6c0
abdulawalemon1 <emondx4@gmail.com>
2022-03-24 21:01:49 +0600

    cart data adding in local storage & displaying in UI done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,8 +5,10 @@ const Cart = (props) => {
 
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price;
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
     }
     const tax = (total * 0.1).toFixed(2);
@@ -16,7 +18,7 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h4>Order summary</h4>
-            <p>Selected Items: {props.cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping: {shipping}</p>
             <p>Tax: {tax}</p>
