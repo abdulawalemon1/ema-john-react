8c9405c0276a31ad7d2de99cdf9716081d6de059
abdulawalemon1 <emondx4@gmail.com>
2022-04-25 16:54:02 +0600

    pagination added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,11 +8,22 @@ import './Shop.css'
 
 const Shop = () => {
 
-    const [products, setProducts] = useProducts();
+
     const [cart, setCart] = useState([]);
     const [pageCount, setPageCount] = useState(0);
     const [page, setPage] = useState(0);
+    const [size, setSize] = useState(10);
+    const [products, setProducts] = useState([]);
 
+    useEffect(() => {
+        fetch(`http://localhost:5000/product?page=${page}&size=${size}`)
+            .then(res => res.json())
+            .then(data => {
+
+                setProducts(data)
+
+            })
+    }, [page, size]);
 
     useEffect(() => {
         fetch('http://localhost:5000/productCount')
@@ -70,6 +81,7 @@ const Shop = () => {
                 }
 
             </div>
+
             <div className="cart-container">
                 <Cart cart={cart}>
                     <Link to="/orders">
@@ -86,6 +98,12 @@ const Shop = () => {
                             {number + 1}
                         </button>)
                 }
+                <select onChange={e => setSize(e.target.value)}>
+                    <option value="5">5</option>
+                    <option value="10" selected>10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                </select>
             </div>
         </div>
     );
