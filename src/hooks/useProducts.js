8f1ef475a14f01efe7426f7687c3e973520a4a4b
abdulawalemon1 <emondx4@gmail.com>
2022-04-25 13:21:37 +0600

    server data loaded+pagination

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 const useProducts = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('products.json')
+        fetch('http://localhost:5000/product')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
